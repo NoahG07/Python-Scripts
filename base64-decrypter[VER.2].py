93f270da88cddcdef315e3a861da97d8a16f9337
Noah Garcia <34785593+NoahG07@users.noreply.github.com>
2018-12-15 20:07:38 -0800

    Update base64-decrypter[VER.2].py

--- base64-decrypter[VER.2].py
+++ base64-decrypter[VER.2].py
@@ -1,21 +1,18 @@
 #! /usr/bin/python3
 import base64
 
-def main():
-    welcome = input("Is Your String Encrypted in Base64? [Y/N]: ")
-    while welcome.upper() == "Y":
-		
-        password = input("What Is The Password You Would Like To Decrypt? ")
-        decrypt = base64.b64decode(password)
+def main():	
+    password = input("What Is The Password You Would Like To Decrypt? ")
+    decrypt = base64.b64decode(password)
 
-        print("Decrypted Password: " + str(decrypt))
-        welcome = input("Do You Have Another Password To Decrypt? ")
+    print("Decrypted Password: " + str(decrypt))
+    welcome = input("Do You Have Another Password To Decrypt? [Yes|No]")
 
-        if welcome.lower() == "yes":
-            main()
-
-        else:
-            print("Thank You!")
-            exit
+    if welcome.lower() == "yes":
+        main()
+    else:
+        print("Thank You!")
+        exit
+	
 if __name__ == "__main__":
     main()
