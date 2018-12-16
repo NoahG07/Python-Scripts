31d1de55702cb0500186d889ed9bef24e117859e
Noah Garcia <34785593+NoahG07@users.noreply.github.com>
2018-12-15 20:03:19 -0800

    Update and rename myip.py to my_ip.py

--- my_ip.py
+++ my_ip.py
@@ -6,7 +6,6 @@ from requests import get
 public_ip = get('https://api.ipify.org').text 
 local_ip = socket.gethostbyname(socket.gethostname()) 
 
-print('\nGetting public and local IP... ') 
-#print('Public IP: ' + public_ip\n'Local IP: ' + local_ip)
+print('\nGetting public and local IP... ')
 print('Public IP: ' + public_ip) 
 print('Local IP: ' + local_ip + '\n') 
