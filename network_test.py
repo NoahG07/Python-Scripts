e6e0914526a2f769a425d1863ef2f2526c2cfded
Noah Garcia <34785593+NoahG07@users.noreply.github.com>
2018-12-15 20:01:28 -0800

    Update network_test.py

--- network_test.py
+++ network_test.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python3
+# This script was made to test a network connection 
+# every 3 minutes, if there are connectivity issues.
 import time, subprocess
 
 count = 0
 Tf = True
 
 while Tf:
-    subprocess.Popen(["ping -c 3 google.com"], shell=True)
+    subprocess.Popen(["ping -c 2 google.com"], shell=True)
     count += 1
     time.sleep(300)
     if count == 5:
