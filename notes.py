df8d68a63fe2a675820c40616c311f6826455e55
Noah Garcia <34785593+NoahG07@users.noreply.github.com>
2018-12-15 20:17:48 -0800

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+
+from sys import argv
+
+try: 
+    script, filename = argv 
+except ValueError: 
+    print('You forgot the filename.')
+
+print ('')
+print ('[*] Welcome User!') 
+print ('[*] This is a simple note taking application.')
+print ('[*] Working File: ' + filename)
+print ('-----------------------------------------------') 
+print ('') 
+
+f = open(filename, 'a')
+
+while True:
+    name = input()
+
+# 'if' what I input is not equal to 'quit', the program will continue to write
+# to the file.
+    if name != 'quit':
+        f.write(str(name) + '\n') 
+# 'else' if quit was entered, the loop will break and close the file.
+    else: 
+        break 
+        f.close() 
