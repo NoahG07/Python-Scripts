6333b68cd184d8bf565054c38ef2e3752ea2b367
Noah <noahgarcia83@gmail.com>
2020-05-30 18:53:36 -0700

    PIP Upgrade Script added

---
+++ pip-upgrade-V2.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import os
+
+
+def upgrade_pip():
+    os.system('start cmd /k python.exe -m pip install --upgrade pip')
+
+
+root = tk.Tk()
+canvas1 = tk.Canvas(root, width=300, height=350, bg='lightsteelblue2',
+                    relief='raised')
+canvas1.pack()
+
+label1 = tk.Label(root, text='Upgrade PIP', bg='lightsteelblue2')
+label1.config(font=('helvetica', 20))
+canvas1.create_window(150, 80, window=label1)
+
+button1 = tk.Button(text='      Upgrade PIP     ', command=upgrade_pip,
+                    bg='green', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150,180, window=button1)
+
+root.mainloop()
